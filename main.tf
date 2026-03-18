115c2c10bac617a586977ce5b797308d84febc6d
El Hadji Diouf <chimerediouf2000@gmail.com>
2026-03-17 21:46:32 -0400

    added public and private subnets

--- main.tf
+++ main.tf
@@ -15,5 +15,7 @@ terraform {
 module "vpc" {
   source         = "./vpc"
   vpc_cidr_block = var.vpc_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
   tags           = local.project_tags
 }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -14,3 +14,44 @@ resource "aws_internet_gateway" "igw" {
   Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-igw"
 })
 }
+
+# CREATING PUBLIC SUBNETS---------------------------------------- --------------------------------------------------------------------------
+resource "aws_subnet" "public_subnet_az_1a" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnet_cidr_block[0]
+  availability_zone = var.availability_zone[0]
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-public-subnet-az-1a"
+})
+}
+
+resource "aws_subnet" "public_subnet_az_1b" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnet_cidr_block[1]
+  availability_zone = var.availability_zone[1]
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-public-subnet-az-1b"
+})
+}
+
+resource "aws_subnet" "private_subnet_az_1a" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnet_cidr_block[2]
+  availability_zone = var.availability_zone[0]
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-subnet-az-1a"
+})
+}
+
+resource "aws_subnet" "private_subnet_az_1b" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnet_cidr_block[3]
+  availability_zone = var.availability_zone[1]
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-subnet-az-1b"
+})
+}
