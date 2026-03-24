20f3880e928978247d8627a74e6cde98269caa84
El Hadji Diouf <chimerediouf2000@gmail.com>
2026-03-24 15:01:35 -0400

    added public and private route tables

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -79,3 +79,118 @@ resource "aws_subnet" "db_subnet_az_1b" {
   Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-db-subnet-az-1b"
 })
 }
+
+# CREATING PUBLIC ROUTE TABLE------------------------------------------------------------------------
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-public-rt"
+})
+}
+
+# CREATING SUBNET ASSOCIATION FOR PUBLIC SUBNET---------------------------------------------------------------------
+resource "aws_route_table_association" "public_association-az-1a" {
+  subnet_id      = aws_subnet.public_subnet_az_1a.id
+  route_table_id = aws_route_table.public-rt.id
+}
+resource "aws_route_table_association" "public_association-az-1b" {
+  subnet_id      = aws_subnet.public_subnet_az_1b.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# CREATING ELASTIC IP FOR NATGATEWAY az 1a----------------------------------------------
+resource "aws_eip" "eip_az_1a" {
+  domain   = "vpc"
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-eip-az-1a"
+})
+}
+
+# CREATING NATGATEWAY az 1a ----------------------------------------------------------------
+resource "aws_nat_gateway" "nat_gw_az_1a" {
+  allocation_id = aws_eip.eip_az_1a.id
+  subnet_id     = aws_subnet.public_subnet_az_1a
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-nat-gw-az-1a"
+})
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.eip_az_1a, aws_subnet.public_subnet_az_1a]
+}
+
+# CREATING PRIVATE ROUTE TABLE az 1a--------------------------------------------------------------------------------------------
+resource "aws_route_table" "private_rt_az_1a" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nat_gw_az_1a.id
+  }
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-rt-az-1a"
+})
+}
+
+#CREATING SUBNET ASSOCIATION FOR PRIVATE ROUTE TABLE FOR AZ 1a ---------------------------------------------------------------------
+resource "aws_route_table_association" "private_association-az-1a" {
+  subnet_id      = aws_subnet.private_subnet_az_1a.id
+  route_table_id = aws_route_table.private_rt_az_1a.id
+}
+resource "aws_route_table_association" "db_association-az-1a" {
+  subnet_id      = aws_subnet.db_subnet_az_1a.id
+  route_table_id = aws_route_table.private_rt_az_1a.id
+}
+
+# CREATING ELASTIC IP FOR NATGATEWAY az 1b----------------------------------------------
+resource "aws_eip" "eip_az_1b" {
+  domain   = "vpc"
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-eip-az-1b"
+})
+}
+
+# CREATING NATGATEWAY az 1b ----------------------------------------------------------------
+resource "aws_nat_gateway" "nat_gw_az_1b" {
+  allocation_id = aws_eip.eip_az_1b.id
+  subnet_id     = aws_subnet.public_subnet_az_1b.id
+
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-nat-gw-az-1b"
+})
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.eip_az_1b, aws_subnet.public_subnet_az_1b]
+}
+
+# CREATING PRIVATE ROUTE TABLE az 1b--------------------------------------------------------------------------------------------
+resource "aws_route_table" "private_rt_az_1b" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nat_gw_az_1b.id
+  }
+  tags =  merge(var.tags, {
+  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-rt-az-1b"
+})
+}
+
+#CREATING SUBNET ASSOCIATION FOR PRIVATE ROUTE TABLE FOR AZ 1b ---------------------------------------------------------------------
+resource "aws_route_table_association" "private_association-az-1b" {
+  subnet_id      = aws_subnet.private_subnet_az_1b.id
+  route_table_id = aws_route_table.private_rt_az_1b.id
+}
+resource "aws_route_table_association" "db_association-az-1b" {
+  subnet_id      = aws_subnet.db_subnet_az_1b.id
+  route_table_id = aws_route_table.private_rt_az_1b.id
+}
