5914c3fd5fdd0b323a32d7cec53afe1dd4abab80
El Hadji Diouf <chimerediouf2000@gmail.com>
2026-04-01 21:55:13 -0400

    correcting syntax errors

--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ module "vpc" {
 module "ec2" {
   source                  = "./ec2"
   vpc_id                  = module.vpc.vpc_id
-  public_subnet_az_1a_id  = module.vpc.public_subnet_az_1a_id
-  private_subnet_az_1a_id = module.vpc.private_subnet_az_1a_id
-  private_subnet_az_1b_id = module.vpc.private_subnet_az_1b_id
+  public_subnet_az_1a_id  = var.public_subnet_az_1a_id
+  private_subnet_az_1a_id = var.private_subnet_az_1a_id
+  private_subnet_az_1b_id = var.private_subnet_az_1b_id
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
